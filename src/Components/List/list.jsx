2b034cadb484f2841c9005754fb4f08b3e4fd472
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-09-10 12:55:27 +0400

    feat(): add functional to delate button

--- src/Components/List/list.jsx
+++ src/Components/List/list.jsx
@@ -1,12 +1,12 @@
 import Button from "../Input/Button/Button";
 import Input from "../Input/Input";
 
-export default function List({ value, edit, delate }) {
+export default function List({ value, edit, delate, onDelate }) {
   return (
     <div>
       <Input value={value} />
       <Button text={edit} />
-      <Button text={delate} />
+      <Button text={delate} handleClick={onDelate} />
     </div>
   );
 }
--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -6,25 +6,37 @@ import List from "../Components/List/list";
 export default function Todo() {
   const [todoList, setTodoList] = useState();
   const [list, setList] = useState([]);
+  const [count, setCount] = useState(0);
   const onHandleChange = (el) => {
     // el.preventDefault();
     setTodoList(el.target.value);
   };
-  console.log(todoList);
+  //   console.log(todoList);
 
   const onHandleClick = () => {
-    setList([todoList, ...list]);
+    setCount(count + 1);
+    setList([{ name: todoList, id: count }, ...list]);
   };
-  console.log("list", list);
-
+  const delateInput = (id) => {
+    setList(list.filter((el) => id !== el.id));
+    console.log("aaaaaaaaa");
+  };
+  console.log(list);
   return (
     <div>
       <Input onChange={(el) => onHandleChange(el)} />
       <Button handleClick={() => onHandleClick()} text="click" />
       {list
-        ? list.map((el) => <List value={el} edit="edit" delate="delate" />)
+        ? list.map((el) => (
+            <List
+              value={el.name}
+              edit="edit"
+              delate="delate"
+              onDelate={() => delateInput(el.id)}
+            />
+          ))
         : null}
-      <p>{todoList}</p>
+      <p></p>
     </div>
   );
 }
